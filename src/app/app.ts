cff4089c6ef5f510b53bbf96478d239d2fc06c65
Thiago Bueno Costa <th.bueno.95@gmail.com>
2025-09-07 22:42:40 -0300

    Feature create component navbar with a conditional if to show (#2)
    * #UPDATE - commit inicial com os arquivos do projeto
    
    * #navBarTask - removing a trash implentation and crafting a new component appointed the NavBar and add configs under a polyfills.ts
    
    * Refactor: Replace old implementation with NavBar component
    
    - Removes the previous inline implementation.
    - Creates a new, reusable NavBar component.
    - Adds polyfills.ts to handle zone.js configuration.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Navbar} from './_components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ngHelloWordAngular');
+  showNavBar: boolean = true;
 }

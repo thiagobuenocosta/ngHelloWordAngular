21ab1685e87c18e2e8858bcdf6893eac535f5840
thiagobuenocosta <th.bueno.95@gmail.com>
2025-09-11 23:29:02 -0300

    feat: Create item-list component to display certificates

---
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {SecondaryButton} from '../secondary-button/secondary-button';
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [
+    SecondaryButton
+  ],
+  templateUrl: './item-certificado.html',
+  styleUrl: './item-certificado.css'
+})
+export class ItemCertificado {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import {Navbar} from './_components/navbar/navbar';
 import {PrimaryButton} from './_components/primary-button/primary-button';
 import {SecondaryButton} from './_components/secondary-button/secondary-button';
+import {ItemCertificado} from './_components/item-certificado/item-certificado';
 import {BaseUi} from './_components/base-ui/base-ui';
 
 @Component({

15b2f9078aa602141a0053f382e2de8aa1d7afda
thiagobuenocosta <th.bueno.95@gmail.com>
2025-09-11 23:28:44 -0300

    style: Establish global margins via base-ui component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import {Navbar} from './_components/navbar/navbar';
 import {PrimaryButton} from './_components/primary-button/primary-button';
 import {SecondaryButton} from './_components/secondary-button/secondary-button';
+import {BaseUi} from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, SecondaryButton],
+  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

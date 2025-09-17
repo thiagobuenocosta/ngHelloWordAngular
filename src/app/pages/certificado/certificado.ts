48fab0e2a2c021dd5a4b99f0d5cec0ca7d4520a8
thiagobuenocosta <th.bueno.95@gmail.com>
2025-09-16 22:52:03 -0300

    Feat: Add a new page visualizer and download certificate

---
+++ src/app/pages/certificado/certificado.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
+
+@Component({
+  selector: 'app-certificado',
+  imports: [
+    SecondaryButton
+  ],
+  templateUrl: './certificado.html',
+  styleUrl: './certificado.css'
+})
+export class Certificado {
+
+}
--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
+// import {SecondaryButton} from '../../_components/secondary-button/secondary-button';
 import {ItemCertificado} from '../../_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-certificados',
   imports: [
-    SecondaryButton,
+    // SecondaryButton,
     ItemCertificado
   ],
   templateUrl: './certificados.html',

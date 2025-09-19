6b969531461833f30488a2b6a15a122eba85133b
thiagobuenocosta <th.bueno.95@gmail.com>
2025-09-18 21:50:59 -0300

    Feat: Add a new page visualizer and download certificate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import {Certificados} from './pages/certificados/certificados';
+import {Certificado} from './pages/certificado/certificado';
+import {CertificadoForm} from './pages/certificado-form/certificado-form';
 
 export const routes: Routes = [
   {
-    path: "certificados",
+    path: "",
     component: Certificados
-  }
+  },{
+    path: "certificados/novo",
+    component: CertificadoForm
+  },{
+    path: "certificado/:id",
+    component: Certificado
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,14 @@
 import { Component, signal } from '@angular/core';
 import {Navbar} from './_components/navbar/navbar';
-// import {PrimaryButton} from './_components/primary-button/primary-button';
-// import {SecondaryButton} from './_components/secondary-button/secondary-button';
-// import {ItemCertificado} from './_components/item-certificado/item-certificado';
 import {BaseUi} from './_components/base-ui/base-ui';
-// import {Certificados} from './pages/certificados/certificados';
-// import {CertificadoForm} from './pages/certificado-form/certificado-form';
-import {Certificado} from './pages/certificado/certificado';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm, Certificado],
-  imports: [Navbar, BaseUi, Certificado],
+  imports: [Navbar, BaseUi, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ngHelloWordAngular');
-  showNavBar: boolean = true;
 }
